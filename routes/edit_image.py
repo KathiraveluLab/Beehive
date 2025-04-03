c5e4db7d65fb126160f6c905c2c73bd9c83ec631
mahmoudnasser1561 <mahmoudnasser1561@gmail.com>
2025-04-03 18:07:02 +0200

    addressed gemini review request changes

--- routes/edit_image.py
+++ routes/edit_image.py
@@ -2,11 +2,11 @@ from flask import Blueprint, flash, session, request, redirect, url_for
 from bson import ObjectId
 from Database.userdatahandler import update_image
 
-edit_image_pb = Blueprint('image', __name__)
+edit_image_pb = Blueprint('edit_image', __name__)
 
 # Edit images uploaded by the user
 @edit_image_pb.route('/edit/<image_id>', methods=['POST'])
-def edit(image_id):
+def edit_image(image_id):
     
     if 'username' in session or 'google_id' in session:
         title = request.form['title']
