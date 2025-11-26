8e71c964f9fb1ee16da349a0593a90c8b72cbf02
Kartikey Gupta <kartikeyf12@gmail.com>
2025-11-26 12:53:03 +0530

    security: remove deprecated X-XSS-Protection; add Content-Security-Policy

--- utils/security_middleware.py
+++ utils/security_middleware.py
@@ -78,10 +78,10 @@ def init_security_middleware(app):
     def add_security_headers(response):
         response.headers['X-Content-Type-Options'] = 'nosniff'
         response.headers['X-Frame-Options'] = 'DENY'
-        response.headers['X-XSS-Protection'] = '1; mode=block'
+        response.headers['Content-Security-Policy'] = "default-src 'self'"
         response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
         return response
-    
+
     return {'csrf': csrf, 'limiter': limiter}
 
 
