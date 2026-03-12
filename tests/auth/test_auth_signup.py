c60d41c0702ccd65e8b73b66dc46fd6a7d5f7d5e
Balaji J <j.balaji2468@gmail.com>
2026-03-12 22:31:45 +0530

    remove duplicates
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

--- tests/auth/test_auth_signup.py
+++ tests/auth/test_auth_signup.py
@@ -22,22 +22,3 @@ def test_complete_signup_success(client, mock_db):
     assert "access_token" in data
     assert "role" in data
     assert data["role"] == "user"
-
-
-def test_request_otp_reset_success(client, mock_db):
-    """POST /api/auth/request-otp"""
-    hashed_pw = bcrypt.hashpw(b"securepassword", bcrypt.gensalt())
-    mock_db.users.insert_one({
-        "email": "test@example.com",
-        "username": "testuser",
-        "password": hashed_pw
-    })
-
-    response = client.post("/api/auth/request-otp", json={
-        "email": "test@example.com",
-        "purpose": "reset"
-    })
-
-    assert response.status_code == 200
-    data = response.get_json()
-    assert data["message"] == "OTP stored (failed to send email)" # since we are not actually sending emails in tests
