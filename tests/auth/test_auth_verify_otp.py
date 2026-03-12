dd43d595ebcf85c6879fc9b104e41d9be5f7d4e4
Balaji J <j.balaji2468@gmail.com>
2026-03-12 20:58:14 +0530

    test: add verify otp test
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

---
+++ tests/auth/test_auth_verify_otp.py
@@ -0,0 +1,39 @@
+import pytest
+import mongomock
+from unittest.mock import patch
+import bcrypt
+from datetime import datetime, timedelta, timezone
+
+def test_verify_otp_success(client, mock_db):
+    """POST /api/auth/verify-otp"""
+    email = "test@example.com"
+    otp = "123456"
+    expires_at = datetime.now(timezone.utc) + timedelta(minutes=5)
+
+    mock_db.email_otps.insert_one({
+        "email": email,
+        "otp": otp,
+        "expires_at": expires_at
+        })
+    
+    response = client.post("/api/auth/verify-otp", json={
+        "email": "test@example.com",
+        "otp": "123456",
+    })
+
+    assert response.status_code == 200 
+    data = response.get_json()
+    assert data["message"] == "OTP verified"
+
+
+def test_request_otp_failure(client, mock_db):
+    """POST /api/auth/request-otp - Not existing user"""
+
+    response = client.post("/api/auth/verify-otp", json={
+        "email": "test@example.com",
+        "otp": "000000",
+    })
+
+    assert response.status_code == 400
+    data = response.get_json()
+    assert data["error"] == "Invalid OTP"
