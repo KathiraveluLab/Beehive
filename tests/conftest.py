c422089445bc7537df63ce7b7a6471a9fdd25b4a
Balaji J <j.balaji2468@gmail.com>
2026-03-12 22:01:50 +0530

    test: add mocks for databses and token
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,24 @@
 import pytest
+import mongomock
+from unittest.mock import patch
 from app import app as flask_app
+
+@pytest.fixture
+def mock_db():
+    client = mongomock.MongoClient()
+    db = client.beehive
+    with patch("routes.auth.get_db", return_value=db), \
+         patch("routes.adminroutes.get_db", return_value=db), \
+         patch("database.userdatahandler.beehive_image_collection", db.images), \
+         patch("database.userdatahandler.beehive_user_collection", db.users):
+        yield db
+
+from utils.jwt_auth import create_access_token
+
+@pytest.fixture
+def admin_token(app):
+    with app.app_context():
+        return create_access_token(user_id="mock_admin_id", role="admin")
 
 @pytest.fixture
 def app():
